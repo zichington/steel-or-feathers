3777c1ba8fa3a5345fd09e791ea8961accad7786
Zichao Zang <zichao.zang@ynap.com>
2019-08-23 13:15:22 +0100

    Updates

---
+++ precache-manifest.6edd8c26214612bfa60ec512d168265a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90f76277f49cefdb88b96bf724f3197",
+    "url": "/steel-or-feathers/index.html"
+  },
+  {
+    "revision": "26d1326407e444a783ea",
+    "url": "/steel-or-feathers/static/css/main.459d4c51.chunk.css"
+  },
+  {
+    "revision": "c2252cd31f6f1d7c5499",
+    "url": "/steel-or-feathers/static/js/2.85d295a8.chunk.js"
+  },
+  {
+    "revision": "26d1326407e444a783ea",
+    "url": "/steel-or-feathers/static/js/main.a899469f.chunk.js"
+  },
+  {
+    "revision": "2ffb9092e7d5d35e003a",
+    "url": "/steel-or-feathers/static/js/runtime~main.8f7947f5.js"
+  }
+]);
